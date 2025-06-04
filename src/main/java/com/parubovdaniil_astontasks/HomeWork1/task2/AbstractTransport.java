f4d105c8513701250cf8bc37646ee37fdbd7ff4f
parubovdaniil <ParubovDaniil@mail.ru>
2025-06-04 20:07:40 +0500

    edits-task2

--- src/main/java/com/parubovdaniil_astontasks/HomeWork1/task2/AbstractTransport.java
+++ src/main/java/com/parubovdaniil_astontasks/HomeWork1/task2/AbstractTransport.java
@@ -1,5 +1,15 @@
 package com.parubovdaniil_astontasks.HomeWork1.task2;
 
-public abstract class AbstractTransport implements Vehicle {
+public abstract class AbstractTransport {
+    public boolean hasWheels() {
+        return false;
+    }
 
+    public boolean hasVint() {
+        return false;
+    }
+
+    public boolean hasWings() {
+        return false;
+    }
 }
--- src/main/java/com/parubovdaniil_astontasks/HomeWork1/task2/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package com.parubovdaniil_astontasks.HomeWork1.task2;
-
-public interface Vehicle {
-    boolean hasWheels();   //колеса
-    boolean hasVint();     //винт
-    boolean hasWings();    //крылья
-}
